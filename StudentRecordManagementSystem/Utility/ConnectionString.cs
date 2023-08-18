9eeef14492cc75cb039a2053aed0d5db9eb7ca97
Saipavanganesh <pavanganeshms@gmail.com>
2023-08-18 16:03:00 +0530

    [Ganesh] add : Student Record Management with CRUD operations using MVC architecture

---
+++ StudentRecordManagementSystem/Utility/ConnectionString.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentRecordManagementSystem.Utility
+{
+    public static class ConnectionString
+    {
+        private static string cName = @"Data Source=LAPTOP-QSRD0QRC\SQLEXPRESS; Initial Catalog=StudentManagement;Integrated Security=True;Connect Timeout=30;Encrypt=False;";
+        
+        public static string CName
+        {
+            get => cName;
+        }
+    }
+}
